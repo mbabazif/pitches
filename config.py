b34e523e3feab5514a25966813500b15af00307a
Florence <mbababziflorence81@gmail.com>
2019-02-26 18:39:59 +0200

    adding the class test config in the config file

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    # MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://saphani:123abc@localhost/watchlist'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
