d719a62a31aaa48d0989d6b833408ef94e868e87
Florence <mbababziflorence81@gmail.com>
2019-02-28 17:25:08 +0200

    adding the pitches html file

--- app/main/views.py
+++ app/main/views.py
@@ -32,17 +32,18 @@ def index():
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username=uname).first()
-    pitches_count = Pitch.count_pitches(uname)
+    # pitches_count = Pitch.count_pitches(uname)
     user_joined = user.date_joined.strftime('%b %d, %Y')
 
     if user is None:
         abort(404)
 
-    return render_template(
-        "profile/profile.html",
-        user=user,
-        pitches=pitches_count,
-        date=user_joined)
+
+#     return render_template(
+#         "profile/profile.html",
+#         user=user,
+#         pitches=pitches_count,
+#         date=user_joined)
 
 
 @main.route('/user/<uname>/update', methods=['GET', 'POST'])
@@ -172,12 +173,12 @@ def pitch(id):
 def user_pitches(uname):
     user = User.query.filter_by(username=uname).first()
     pitches = Pitch.query.filter_by(user_id=user.id).all()
-    pitches_count = Pitch.count_pitches(uname)
+    # pitches_count = Pitch.count_pitches(uname)
     user_joined = user.date_joined.strftime('%b %d, %Y')
 
     return render_template(
         "profile/pitches.html",
         user=user,
         pitches=pitches,
-        pitches_count=pitches_count,
+        # pitches_count=pitches_count,
         date=user_joined)
