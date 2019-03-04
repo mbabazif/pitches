f873bea5f1aac23f163a21e190c336317362bb03
Florence <mbababziflorence81@gmail.com>
2019-03-04 13:49:58 +0200

    adding the edit profile

--- app/main/views.py
+++ app/main/views.py
@@ -32,13 +32,11 @@ def index():
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username=uname).first()
-    # pitches_count = Pitch.count_pitches(uname)
-    user_joined = user.date_joined.strftime('%b %d, %Y')
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user=user, date=user_joined)
+    return render_template("profile/profile.html", user=user)
 
 
 @main.route('/user/<uname>/update', methods=['GET', 'POST'])
@@ -46,9 +44,9 @@ def profile(uname):
 def update_profile(uname):
     user = User.query.filter_by(username=uname).first()
     if user is None:
-        # abort(404)
+        abort(404)
 
-        form = UpdateProfile()
+    form = UpdateProfile()
 
     if form.validate_on_submit():
         user.bio = form.bio.data
@@ -59,6 +57,48 @@ def update_profile(uname):
         return redirect(url_for('.profile', uname=user.username))
 
     return render_template('profile/update.html', form=form)
+
+
+# @main.route('/user/<uname>')
+# def profile(uname):
+#     user = User.query.filter_by(username=uname).first()
+#     # pitches_count = Pitch.count_pitches(uname)
+#     user_joined = user.date_joined.strftime('%b %d, %Y')
+
+#     if user is None:
+#         abort(404)
+
+#     return render_template("profile/profile.html", user=user, date=user_joined)
+
+# @main.route('/user/<uname>/update', methods=['GET', 'POST'])
+# @login_required
+# def update_profile(uname):
+#     user = User.query.filter_by(username=uname).first()
+#     if user is None:
+#         abort(404)
+
+#         form = UpdateProfile()
+
+#     if form.validate_on_submit():
+#         user.bio = form.bio.data
+
+#         db.session.add(user)
+#         db.session.commit()
+
+#         return redirect(url_for('.profile', uname=user.username))
+
+#     return render_template('profile/update.html', form=form)
+
+# @main.route('/user/<uname>/update/pic', methods=['POST'])
+# @login_required
+# def update_pic(uname):
+#     user = User.query.filter_by(username=uname).first()
+#     if 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         path = f'photos/{filename}'
+#         user.profile_pic_path = path
+#         db.session.commit()
+#     return redirect(url_for('main.profile', uname=uname))
 
 
 @main.route('/user/<uname>/update/pic', methods=['POST'])
